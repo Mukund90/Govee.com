90e676a5270ba07d8ed8a04631eda60e6cada23b
Mukund jha <jhamukund986@gmail.com>
2024-12-31 19:54:38 +0530

    msg

---
+++ dist/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"], 
+  theme: {
+    extend: {
+      colors:{
+        customblue : '#18a2e4'
+      }
+    },
+  },
+  plugins: [],
+}
